d1522682f7618b3a4b4297a3ab8cecadd0bb929e
fromanint <RomanInteractive@FROMAN.local>
2016-04-15 12:22:45 -0500

    first commit

---
+++ Assets/Scripts/Player/CharacterMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMove : MonoBehaviour {
+
+	public int speed;
+
+
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	public void MoveFront(){
+		
+		rb.velocity = speed * transform.forward; 
+	}
+	public void MoveBack(){
+		rb.velocity = -speed * transform.forward; 
+	}
+	public void Stop(){
+		rb.velocity = new Vector3(0,0,0);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(CharacterMove))]
+public class PlayerControl : MonoBehaviour {
+
+	CharacterMove cm;
+
+	// Use this for initialization
+	void Start () {
+		cm = GetComponent<CharacterMove> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal")==0)
+			cm.Stop();
+		if(Input.GetAxis("Horizontal")>0)
+			cm.MoveFront();
+		if(Input.GetAxis("Horizontal")<0)
+			cm.MoveBack();
+
+			
+	}
+}
